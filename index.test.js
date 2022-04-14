a87c28925f08aee1c6b56653667c15ab6f93d90e
Mario González <mario@mariogl.com>
2022-04-14 16:53:33 +0200

    I don't need you Jest

---
+++ index.test.js
@@ -0,0 +1,15 @@
+const sum = (number1, number2) => number1 + number1;
+
+describe("Given a sum function", () => {
+  describe("When it receives 3 and 2", () => {
+    test("Then it should return 5", () => {
+      const number1 = 3;
+      const number2 = 2;
+      const expectedResult = 5;
+
+      const result = sum(number1, number2);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+});
